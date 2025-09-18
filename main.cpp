7c4139e0dbfacc688b2cbae6c539884d0ec99794
rylander925 <rylander925@gmail.com>
2025-09-17 22:13:38 -0700

    added fillArray method

--- main.cpp
+++ main.cpp
@@ -8,16 +8,31 @@ IDE Used: Visual Studio Code
 #include <iostream>
 #include <algorithm>
 #include <numeric>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
 const int SIZE = 30;
+const string FILENAME = "data.txt";
 
 void fillArray(array<double, SIZE>& arr);
 
 void displayArray(const array<double, SIZE>& arr);
 
-
 int main() {
 
+}
+
+void fillArray(array<double, SIZE>& arr) {
+    ifstream infile;
+    infile.open(FILENAME);
+    if (!infile.good()) {
+        cout << "ERROR: File not found: \"" << FILENAME  << "\"" << endl;
+        throw ios_base::failure("Invalid file name");
+    }
+    for(int i = 0; i < SIZE; i++) {
+        infile >> arr.at(i);
+    }
+    infile.close();
 }
