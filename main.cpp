15037166cd41df9c3f6469e0420bcec9fe136664
rylander925 <rylander925@gmail.com>
2025-09-17 22:07:03 -0700

    added stubs

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*
+COMSC-210 | Lab 12 | Rylan Der
+IDE Used: Visual Studio Code
+*/
+
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+const int SIZE = 30;
+
+void fillArray(array<double, SIZE>& arr);
+
+void displayArray(const array<double, SIZE>& arr);
+
+
+int main() {
+
+}
