b2722851a31cb5b050419af427c2018c8696771b
rylander925 <rylander925@gmail.com>
2025-09-17 22:32:25 -0700

    added average and median functions

--- main.cpp
+++ main.cpp
@@ -19,12 +19,19 @@ void fillArray(array<T, size>& arr, string filename);
 template<typename T, size_t size>
 void displayArray(const array<T, size>& arr);
 
+template<typename T, size_t size>
+double averageArray(const array<T, size>& arr);
+
+template<typename T, size_t size>
+double medianArray(const array<T, size>& arr);
+
 int main() {
     array<int, 30> nums;
     const string FILENAME = "data.txt";
     fillArray(nums, FILENAME);
     displayArray(nums);
-
+    cout << "Average: " << averageArray(nums) << endl;
+    cout << "Median: " << medianArray(nums) << endl;
 }
 
 template<typename T, size_t size>
@@ -48,4 +55,16 @@ void displayArray(const array<T, size>& arr) {
     }
     cout << endl;
 
+}
+
+template<typename T, size_t size>
+double medianArray(const array<T, size>& arr) {
+    array<T, size> copy = arr;
+    sort(copy.begin(), copy.end());
+    return (size % 2 == 0) ? ((copy.at(size / 2) + copy.at((size / 2) - 1)) / 2.0) : (copy.at(size / 2));
+}
+
+template<typename T, size_t size>
+double averageArray(const array<T, size>& arr) {
+    return accumulate(arr.begin(), arr.end(), 0) * 1.0 / size;
 }
