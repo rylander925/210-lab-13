a7198fa467f148c0d6febb7a1217ae4d1934d99c
rylander925 <rylander925@gmail.com>
2025-09-17 22:19:58 -0700

    Added basic display, changed to use templates

--- main.cpp
+++ main.cpp
@@ -13,26 +13,39 @@ IDE Used: Visual Studio Code
 
 using namespace std;
 
-const int SIZE = 30;
-const string FILENAME = "data.txt";
+template<typename T, size_t size>
+void fillArray(array<T, size>& arr, string filename);
 
-void fillArray(array<double, SIZE>& arr);
-
-void displayArray(const array<double, SIZE>& arr);
+template<typename T, size_t size>
+void displayArray(const array<T, size>& arr);
 
 int main() {
+    array<int, 30> nums;
+    const string FILENAME = "data.txt";
+    fillArray(nums, FILENAME);
+    displayArray(nums);
 
 }
 
-void fillArray(array<double, SIZE>& arr) {
+template<typename T, size_t size>
+void fillArray(array<T, size>& arr, string filename) {
     ifstream infile;
-    infile.open(FILENAME);
+    infile.open(filename);
     if (!infile.good()) {
-        cout << "ERROR: File not found: \"" << FILENAME  << "\"" << endl;
+        cout << "ERROR: File not found: \"" << filename  << "\"" << endl;
         throw ios_base::failure("Invalid file name");
     }
-    for(int i = 0; i < SIZE; i++) {
+    for(int i = 0; i < size; i++) {
         infile >> arr.at(i);
     }
     infile.close();
+}
+
+template<typename T, size_t size>
+void displayArray(const array<T, size>& arr) {
+    for(int i = 0; i < size; i++) {
+        cout << arr.at(i) << " ";
+    }
+    cout << endl;
+
 }
