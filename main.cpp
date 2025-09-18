2196f513dbef8079e041e8954eabaae8106a8903
rylander925 <rylander925@gmail.com>
2025-09-18 13:16:53 -0700

    Modified fill & display to use groceries

--- main.cpp
+++ main.cpp
@@ -14,6 +14,8 @@ IDE Used: Visual Studio Code
 
 using namespace std;
 
+const int IGNORE_CHARS = 100; //Characters ignored by istream.ignore()
+
 struct Grocery {
     string name;
     double price;
@@ -25,11 +27,14 @@ bool operator<=(const Grocery& g1, const Grocery& g2);
 bool operator>(const Grocery& g1, const Grocery& g2);
 bool operator>=(const Grocery& g1, const Grocery& g2);
 
-template<typename T, size_t size>
-void fillArray(array<T, size>& arr, string filename);
+/**
+ * @note File line must be "Name price"
+ */
+template<size_t size>
+void fillGroceries(array<Grocery, size>& arr, string filename);
 
-template<typename T, size_t size>
-void displayArray(const array<T, size>& arr);
+template<size_t size>
+void displayGroceries(const array<Grocery, size>& arr);
 
 template<typename T, size_t size>
 double averageArray(const array<T, size>& arr);
@@ -41,12 +46,12 @@ void coutLine(int size = 50, char lineChar = '=');
 
 int main() {
     const string FILENAME = "data.txt";
+    const int SIZE = 5;
     string input;
-    array<int, 30> nums;
-    fillArray(nums, FILENAME);
-
+    array<Grocery, SIZE> groceries;
+    fillGroceries(groceries, FILENAME);
     cout << "Displaying data: " << endl;
-    displayArray(nums);
+    displayGroceries(groceries);
 
     
 }
@@ -56,24 +61,27 @@ void coutLine(int size, char lineChar) {
     cout << setw(size) << setfill(lineChar) << "" << setfill(prevFillChar) << endl;
 }
 
-template<typename T, size_t size>
-void fillArray(array<T, size>& arr, string filename) {
+template<size_t size>
+void fillGroceries(array<Grocery, size>& arr, string filename) {
     ifstream infile;
     infile.open(filename);
     if (!infile.good()) {
         cout << "ERROR: File not found: \"" << filename  << "\"" << endl;
         throw ios_base::failure("Invalid file name");
     }
-    for(int i = 0; i < size; i++) {
-        infile >> arr.at(i);
+    for(Grocery& g : arr) {
+        getline(infile, g.name);
+        infile >> g.price;
+        infile.ignore(IGNORE_CHARS, '\n');
     }
     infile.close();
 }
 
-template<typename T, size_t size>
-void displayArray(const array<T, size>& arr) {
-    for(int i = 0; i < size; i++) {
-        cout << arr.at(i) << " ";
+template<size_t size>
+void displayGroceries(const array<Grocery, size>& arr) {
+    cout << fixed << setprecision(2);
+    for(Grocery g : arr) {
+        cout << g.name << ": $" << g.price << "  ";
     }
     cout << endl;
 
@@ -91,7 +99,7 @@ double averageArray(const array<T, size>& arr) {
     return accumulate(arr.begin(), arr.end(), 0) * 1.0 / size;
 }
 
-//Define comparison operators to compare by price
+//Define comparison operators to compare by prices
 bool operator<(const Grocery& g1, const Grocery& g2) {
     return g1.price < g2.price;
 }
